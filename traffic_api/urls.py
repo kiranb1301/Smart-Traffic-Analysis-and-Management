5d0e7d26f300cddc97fb38ffb8d60f145f4b419e
Kiran Bade <badekiran67@gmail.com>
2025-06-10 18:58:19 +0530

    Added changed urls

--- traffic_api/urls.py
+++ traffic_api/urls.py
@@ -12,11 +12,11 @@ from .views import (
 
 urlpatterns = [
     path('', index, name='index'),
-    path('/smarttraffic/api/traffic/', TrafficRecordListCreate.as_view(), name='traffic-data'),
-    path('/smarttraffic/api/upload/', CSVUploadView.as_view(), name='upload-csv'),
-    path('/smarttraffic/api/predict/', PredictTrafficAPIView.as_view(), name='predict-traffic'),
-    path('/smarttraffic/api/optimize/', OptimizeTrafficAPIView.as_view(), name='optimize-traffic'),
-    path('/smarttraffic/api/congestion/', CongestionClusterAPIView.as_view(), name='congestion-detection'),
-    path('/smarttraffic/api/realtime/process/', RealTimeProcessorAPIView.as_view(), name='realtime-processing'),
-    path("/smarttraffic/api/run-simulation/", run_simulation, name="run_simulation"),
+    path('smarttraffic/api/traffic/', TrafficRecordListCreate.as_view(), name='traffic-data'),
+    path('smarttraffic/api/upload/', CSVUploadView.as_view(), name='upload-csv'),
+    path('smarttraffic/api/predict/', PredictTrafficAPIView.as_view(), name='predict-traffic'),
+    path('smarttraffic/api/optimize/', OptimizeTrafficAPIView.as_view(), name='optimize-traffic'),
+    path('smarttraffic/api/congestion/', CongestionClusterAPIView.as_view(), name='congestion-detection'),
+    path('smarttraffic/api/realtime/process/', RealTimeProcessorAPIView.as_view(), name='realtime-processing'),
+    path('smarttraffic/api/run-simulation/', run_simulation, name='run_simulation'),
 ]
