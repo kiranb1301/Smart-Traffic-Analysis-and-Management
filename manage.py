3e8fdef611ff843c9ba49bb8a71192a0dd46cc68
Kiran Bade <badekiran67@gmail.com>
2025-06-10 18:12:52 +0530

    Added Ports

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management import execute_from_command_line
 
 def main():
     """Run administrative tasks."""
@@ -16,7 +16,13 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traffic_project.settings')
+    
+    # Set the correct port dynamically from the environment variable (PORT) provided by Render
+    port = os.environ.get('PORT', '8000')  # Default to 8000 if not set
+    
+    # Execute Django management command with the correct port binding
+    execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
 
 if __name__ == '__main__':
     main()
